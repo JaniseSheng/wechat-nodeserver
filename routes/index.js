84fed91cfece7b28c2d87d94d2cd4e53cb455a5e
shengxx <janisesheng@cform.io>
2017-03-04 21:13:13 +0800

    signaturen module.exports

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const multer = require('multer');
 const config = require('../config');
 //关联主程序
-const VerificationService = require('../wechat_manage/signature');
+const signature = require('../wechat_manage/signature');
 
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    VerificationService(req, res, next);
+    signature.VerificationService(req, res, next);
 
 });
 
--- wechat_manage/signature.js
+++ wechat_manage/signature.js
@@ -1,13 +1,15 @@
 const qs=require('qs');
 const TOKEN='access_token';
 
-
-export const VerificationService = (req, res, next)=>{
-    const params = req.query;
-    const key=[TOKEN,params.timestamp,params.nonce].sort().join('');
-    //将token （自己设置的） 、timestamp（时间戳）、nonce（随机数）三个参数进行字典排序
-    const sha1=require('crypto').createHash('sha1');
-    //将上面三个字符串拼接成一个字符串再进行sha1加密
-    sha1.update(key);
-    return sha1.digest('hex') == params.signature;
-};
+module.exports = {
+    VerificationService: (req, res, next)=>{
+        const params = req.query;
+        console.log(params);
+        const key=[TOKEN,params.timestamp,params.nonce].sort().join('');
+        //将token （自己设置的） 、timestamp（时间戳）、nonce（随机数）三个参数进行字典排序
+        const sha1=require('crypto').createHash('sha1');
+        //将上面三个字符串拼接成一个字符串再进行sha1加密
+        sha1.update(key);
+        return sha1.digest('hex') == params.signature;
+    }
+}
